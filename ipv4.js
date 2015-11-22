affbc15e597c96675786026c9163e7700ab11ddf
unknown <mike.ralphson@gmail.com>
2015-11-22 02:58:35 +0000

    IPv4 tool and associated 256-word dictionary v0.1

---
+++ ipv4.js
@@ -0,0 +1,72 @@
+var fs = require('fs');
+var crypto = require('crypto');
+
+const sha1 = 'f607be85dfea2cdb666f9761bf04f5de398bb8cd';
+const ERR_WYTAW = "Watchoo talkin about Willis?";
+
+var input = [];
+
+function toAddress(phrase) {
+	words = phrase.toLocaleLowerCase().split('.');
+	if (words.length==4) {
+		octets = '';
+		for (var i=0;i<4;i++) {
+			octets += (octets ? '.' : '') + input.indexOf(words[i]);
+		}
+		return octets;
+	}
+	else {
+		return ERR_WYTAW;
+	}
+}
+
+function toPhrase(address) {
+	octets = address.split('.');
+	if (octets.length==4) {
+		phrase = '';
+		for (var i=0;i<4;i++) {
+			octet = parseInt(octets[i],10);
+			phrase += (phrase ? '.' : '') + input[octet];
+		}
+		return phrase;
+	}
+	else {
+		return ERR_WYTAW;
+	}
+}
+
+if (process.argv.length>2) {
+	instr = fs.readFileSync('./dict/words256','utf8');
+	var shasum = crypto.createHash('sha1');
+	shasum.update(instr);
+	check = shasum.digest('hex');
+	if (sha1 != check) {
+		console.log('Your dictionary is corrupt: '+check);
+	}
+	else {
+		input = instr.split('\n');
+		if (input.length<256) {
+			console.log('This should never happen');
+		}
+		else {
+			parm = process.argv[2];
+			if (parm == 'localhost') {
+				console.log('127.0.0.1');
+			}
+			else if (parm == '127.0.0.1') {
+				console.log('localhost');
+			}
+			else {
+				if ('0123456789'.indexOf(parm[0])>=0) {
+					console.log(toPhrase(parm));
+				}
+				else {
+					console.log(toAddress(parm));
+				}
+			}
+		}
+	}
+}
+else {
+	console.log('Usage: '+process.argv[1]+' addr|localhost|phrase');
+}
